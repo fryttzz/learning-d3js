916aa0bc9ce54133769d960c024e2c6270db99d3
Matheus Santos <matheusps0012@gmail.com>
2022-02-18 17:58:05 -0300

    Add - Tests For New Gear Chart

--- ScrimbaCourse/7creatingSvgs/index.js
+++ ScrimbaCourse/7creatingSvgs/index.js
@@ -1,5 +1,5 @@
 //javascript
-var svgWidth = 600,
+var svgWidth = 700,
     svgHeight = 500;
 
 var svg = d3.select("svg")
@@ -7,23 +7,125 @@ var svg = d3.select("svg")
     .attr("height", svgHeight)
     .attr("class", "svg-container")
 
-var line = svg.append("line")
-    .attr("x1", 100)
-    .attr("x2", 500)
+// var ida = svg.append("line")
+//     .attr("x1", 20)
+//     .attr("x2", 270)
+//     .attr("y1", 450)
+//     .attr("y2", 50)
+//     .attr("stroke", "red")
+//     .attr("stroke-width", 3)
+
+// var volta = svg.append("line")
+//     .attr("x1", 580)
+//     .attr("x2", 330)
+//     .attr("y1", 450)
+//     .attr("y2", 50)
+//     .attr("stroke", "red")
+//     .attr("stroke-width", 3)
+
+// var tempo_terminal_b = svg.append("line")
+//     .attr("x1", 330)
+//     .attr("x2", 270)
+//     .attr("y1", 50)
+//     .attr("y2", 50)
+//     .attr("stroke", "red")
+//     .attr("stroke-width", 3)
+
+// var tempo_terminal_a = svg.append("line")
+//     .attr("x1", 650)
+//     .attr("x2", 580)
+//     .attr("y1", 450)
+//     .attr("y2", 450)
+//     .attr("stroke", "red")
+//     .attr("stroke-width", 3)
+
+var top = svg.append("line")
+    .attr("x1", 700)
+    .attr("x2", 0)
     .attr("y1", 50)
     .attr("y2", 50)
-    .attr("stroke", "red")
-    .attr("stroke-width", 5)
+    .attr("stroke", "black")
+    .attr("stroke-width", 1)
 
-var rect = svg.append("rect")
-    .attr("x", 100)
-    .attr("y", 100)
-    .attr("width", 200)
-    .attr("height", 100)
-    .attr("fill", "#9B95FF")
+var bottom = svg.append("line")
+    .attr("x1", 700)
+    .attr("x2", 0)
+    .attr("y1", 450)
+    .attr("y2", 450)
+    .attr("stroke", "black")
+    .attr("stroke-width", 1)
 
-var circle = svg.append("circle")
-    .attr("cx", 200)
-    .attr("cy", 300)
-    .attr("r", 80)
-    .attr("fill", "#7CE8D5")
+var data = [
+    //carro 1
+    { car: 1, xpoint: 100, ypoint: 450 },
+    { car: 1, xpoint: 200, ypoint: 50 },
+
+    { car: 1, xpoint: 200, ypoint: 50 },
+    { car: 1, xpoint: 300, ypoint: 450 },
+
+    { car: 1, xpoint: 300, ypoint: 450 },
+    { car: 1, xpoint: 400, ypoint: 50 },
+
+    { car: 1, xpoint: 430, ypoint: 50 },
+    { car: 1, xpoint: 530, ypoint: 450 },
+
+    { car: 1, xpoint: 560, ypoint: 450 },
+    { car: 1, xpoint: 660, ypoint: 50 },
+
+    //carro 2
+    { car: 2, xpoint: 150, ypoint: 450 },
+    { car: 2, xpoint: 250, ypoint: 50 },
+
+    { car: 2, xpoint: 250, ypoint: 50 },
+    { car: 2, xpoint: 350, ypoint: 450 },
+
+    { car: 2, xpoint: 350, ypoint: 450 },
+    { car: 2, xpoint: 450, ypoint: 50 },
+
+    { car: 2, xpoint: 480, ypoint: 50 },
+    { car: 2, xpoint: 580, ypoint: 450 },
+
+    { car: 2, xpoint: 610, ypoint: 450 },
+    { car: 2, xpoint: 710, ypoint: 50 },
+
+    //carro 3
+    { car: 3, xpoint: 150, ypoint: 50 },
+    { car: 3, xpoint: 250, ypoint: 450 },
+
+    { car: 3, xpoint: 250, ypoint: 450 },
+    { car: 3, xpoint: 350, ypoint: 50 },
+
+    { car: 3, xpoint: 350, ypoint: 50 },
+    { car: 3, xpoint: 450, ypoint: 450 },
+
+    { car: 3, xpoint: 480, ypoint: 450 },
+    { car: 3, xpoint: 580, ypoint: 50 },
+
+    { car: 3, xpoint: 610, ypoint: 50 },
+    { car: 3, xpoint: 710, ypoint: 450 },
+];
+
+var sumstat = d3.groups(data, d => d.car);
+var mediaName = sumstat.map(d => d[0])
+
+var color = d3.scaleOrdinal().domain(mediaName).range(['#984EA3', '#FF7F00', '#E41A1C', '#377EB8', '#999999', '#FFFF33', '#A65628', '#4DAF4A', ])
+
+var line = d3.line()
+    .x((p) => p.xpoint)
+    .y((p) => p.ypoint)
+    .curve(d3.curveCatmullRom.alpha(0.29))
+
+const lines = svg.selectAll("lines")
+    .data(sumstat)
+    .enter()
+    .append("g");
+
+lines.append("path")
+    .attr('fill', 'none')
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .attr('stroke', d => color(d[0]))
+    .attr('stroke-width', 3)
+    .attr("d", function(d) {
+        return line(d[1]);
+    });
--- ScrimbaCourse/9creatingLineChart/index.js
+++ ScrimbaCourse/9creatingLineChart/index.js
@@ -3,16 +3,16 @@ const data = [{
         "itinerario": "Ref.Mal.Floriano/Eldorado/Serrano",
         "carro": "402",
         "reserva": "605",
-        "saida_planejado": "09:05",
-        "saida": "09:09",
-        "diferenca": "-6163:5:23:56",
-        "entrada": "9:44",
-        "tempo_viagem1": "35m",
-        "tempo_planejado1": "40m",
-        "tempo_parado1": "2m",
-        "tempo_volta": null,
-        "tempo_planejado2": null,
-        "tempo_parado2": null,
+        "saida_planejado": 545, //09:05
+        "saida": 549,
+        "diferenca": 4,
+        "entrada": 584,
+        "tempo_viagem1": 35,
+        "tempo_planejado1": 40,
+        "tempo_parado1": 2,
+        "tempo_volta": 0,
+        "tempo_planejado2": 0,
+        "tempo_parado2": 0,
         "sentido": 1
     },
     {
@@ -20,16 +20,16 @@ const data = [{
         "itinerario": "Serrano/Andreazza/Marechal Floriano",
         "carro": "402",
         "reserva": "605",
-        "saida_planejado": "09:45",
-        "saida": "09:46",
-        "diferenca": "-6163:5:23:59",
-        "entrada": "10:19",
-        "tempo_viagem": "33m",
-        "tempo_planejado1": "35m",
-        "tempo_parado1": "2m",
-        "tempo_volta": "68m",
-        "tempo_planejado2": "75m",
-        "tempo_parado2": "7m",
+        "saida_planejado": 585, //09:45
+        "saida": 586,
+        "diferenca": 1,
+        "entrada": 619,
+        "tempo_viagem": 33,
+        "tempo_planejado1": 35,
+        "tempo_parado1": 2,
+        "tempo_volta": 68,
+        "tempo_planejado2": 75,
+        "tempo_parado2": 7,
         "sentido": 2
     },
     {
@@ -37,16 +37,16 @@ const data = [{
         "itinerario": "Ref.Mal.Floriano/Eldorado/Serrano",
         "carro": "402",
         "reserva": "605",
-        "saida_planejado": "10:20",
-        "saida": "10:21",
-        "diferenca": "-6163:5:23:59",
-        "entrada": "10:57",
-        "tempo_viagem": "36m",
-        "tempo_planejado1": "40m",
-        "tempo_parado1": "3m",
-        "tempo_volta": null,
-        "tempo_planejado2": null,
-        "tempo_parado2": null,
+        "saida_planejado": 620,
+        "saida": 621,
+        "diferenca": 1,
+        "entrada": 657,
+        "tempo_viagem": 36,
+        "tempo_planejado1": 40,
+        "tempo_parado1": 3,
+        "tempo_volta": 0,
+        "tempo_planejado2": 0,
+        "tempo_parado2": 0,
         "sentido": 1
     },
     {
@@ -54,16 +54,16 @@ const data = [{
         "itinerario": "Serrano/Andreazza/Marechal Floriano",
         "carro": "402",
         "reserva": "605",
-        "saida_planejado": "11:00",
-        "saida": "11:00",
-        "diferenca": "-6163:6",
-        "entrada": "11:32",
-        "tempo_viagem": "32m",
-        "tempo_planejado1": "35m",
-        "tempo_parado1": "2m",
-        "tempo_volta": "68m",
-        "tempo_planejado2": "75m",
-        "tempo_parado2": "7m",
+        "saida_planejado": 660,
+        "saida": 660,
+        "diferenca": 0,
+        "entrada": 692,
+        "tempo_viagem": 32,
+        "tempo_planejado1": 35,
+        "tempo_parado1": 2,
+        "tempo_volta": 68,
+        "tempo_planejado2": 75,
+        "tempo_parado2": 7,
         "sentido": 2
     },
     {
@@ -71,16 +71,16 @@ const data = [{
         "itinerario": "Ref.Mal.Floriano/Trav.Leopoldina/Serrano",
         "carro": "402",
         "reserva": "605",
-        "saida_planejado": "11:35",
-        "saida": "11:34",
-        "diferenca": "-6163:6:0:01",
-        "entrada": "12:04",
-        "tempo_viagem": "30m",
-        "tempo_planejado1": "45m",
-        "tempo_parado1": "16m",
-        "tempo_volta": null,
-        "tempo_planejado2": null,
-        "tempo_parado2": null,
+        "saida_planejado": 695,
+        "saida": 694,
+        "diferenca": -1,
+        "entrada": 724,
+        "tempo_viagem": 30,
+        "tempo_planejado1": 45,
+        "tempo_parado1": 16,
+        "tempo_volta": 0,
+        "tempo_planejado2": 0,
+        "tempo_parado2": 0,
         "sentido": 1
     },
     {
@@ -88,16 +88,16 @@ const data = [{
         "itinerario": "Serrano/Eldorado/Mal.Floriano",
         "carro": "402",
         "reserva": "605",
-        "saida_planejado": "12:20",
-        "saida": "12:20",
-        "diferenca": "-6163:6",
-        "entrada": "12:54",
-        "tempo_viagem": "34m",
-        "tempo_planejado1": "35m",
-        "tempo_parado1": "1m",
-        "tempo_volta": "64m",
-        "tempo_planejado2": "80m",
-        "tempo_parado2": "16m",
+        "saida_planejado": 740,
+        "saida": 740,
+        "diferenca": 0,
+        "entrada": 774,
+        "tempo_viagem": 34,
+        "tempo_planejado1": 35,
+        "tempo_parado1": 1,
+        "tempo_volta": 64,
+        "tempo_planejado2": 80,
+        "tempo_parado2": 16,
         "sentido": 2
     },
     {
@@ -105,16 +105,16 @@ const data = [{
         "itinerario": "Ref.Mal.Floriano/Eldorado/Serrano",
         "carro": "402",
         "reserva": "605",
-        "saida_planejado": "12:55",
-        "saida": "12:55",
-        "diferenca": "-6163:6",
-        "entrada": "13:25",
-        "tempo_viagem": "30m",
-        "tempo_planejado1": "40m",
-        "tempo_parado1": "11m",
-        "tempo_volta": null,
-        "tempo_planejado2": null,
-        "tempo_parado2": null,
+        "saida_planejado": 775,
+        "saida": 775,
+        "diferenca": 0,
+        "entrada": 805,
+        "tempo_viagem": 30,
+        "tempo_planejado1": 40,
+        "tempo_parado1": 11,
+        "tempo_volta": 0,
+        "tempo_planejado2": 0,
+        "tempo_parado2": 0,
         "sentido": 1
     },
     {
@@ -122,16 +122,16 @@ const data = [{
         "itinerario": "Serrano/Andreazza/Marechal Floriano",
         "carro": "402",
         "reserva": "605",
-        "saida_planejado": "13:35",
-        "saida": "13:36",
-        "diferenca": "-6163:5:23:59",
-        "entrada": "14:11",
-        "tempo_viagem": "35m",
-        "tempo_planejado1": "35m",
-        "tempo_parado1": "2m",
-        "tempo_volta": "65m",
-        "tempo_planejado2": "75m",
-        "tempo_parado2": "10m",
+        "saida_planejado": 815,
+        "saida": 816,
+        "diferenca": 1,
+        "entrada": 851,
+        "tempo_viagem": 35,
+        "tempo_planejado1": 35,
+        "tempo_parado1": 2,
+        "tempo_volta": 65,
+        "tempo_planejado2": 75,
+        "tempo_parado2": 10,
         "sentido": 2
     },
     {
@@ -139,16 +139,16 @@ const data = [{
         "itinerario": "Ref.Mal.Floriano/Eldorado/Serrano",
         "carro": "402",
         "reserva": "605",
-        "saida_planejado": "14:10",
-        "saida": "14:13",
-        "diferenca": "-6163:5:23:57",
-        "entrada": "14:47",
-        "tempo_viagem": "34m",
-        "tempo_planejado1": "40m",
-        "tempo_parado1": "4m",
-        "tempo_volta": null,
-        "tempo_planejado2": null,
-        "tempo_parado2": null,
+        "saida_planejado": 850,
+        "saida": 853,
+        "diferenca": 3,
+        "entrada": 887,
+        "tempo_viagem": 34,
+        "tempo_planejado1": 40,
+        "tempo_parado1": 4,
+        "tempo_volta": 0,
+        "tempo_planejado2": 0,
+        "tempo_parado2": 0,
         "sentido": 1
     },
     {
@@ -156,16 +156,16 @@ const data = [{
         "itinerario": "Serrano/Andreazza/Marechal Floriano",
         "carro": "402",
         "reserva": "605",
-        "saida_planejado": "14:50",
-        "saida": "14:51",
-        "diferenca": "-6163:5:23:59",
-        "entrada": "15:29",
-        "tempo_viagem": "38m",
-        "tempo_planejado1": "35m",
-        "tempo_parado1": "2m",
-        "tempo_volta": "72m",
-        "tempo_planejado2": "75m",
-        "tempo_parado2": "3m",
+        "saida_planejado": 890,
+        "saida": 890,
+        "diferenca": 1,
+        "entrada": 929,
+        "tempo_viagem": 38,
+        "tempo_planejado1": 35,
+        "tempo_parado1": 2,
+        "tempo_volta": 72,
+        "tempo_planejado2": 75,
+        "tempo_parado2": 3,
         "sentido": 2
     },
     {
@@ -173,16 +173,16 @@ const data = [{
         "itinerario": "Ref.Mal.Floriano/Eldorado/Serrano",
         "carro": "402",
         "reserva": "605",
-        "saida_planejado": "15:25",
-        "saida": "15:31",
-        "diferenca": "-6163:5:23:54",
-        "entrada": "16:09",
-        "tempo_viagem": "38m",
-        "tempo_planejado1": "45m",
-        "tempo_parado1": "3m",
-        "tempo_volta": null,
-        "tempo_planejado2": null,
-        "tempo_parado2": null,
+        "saida_planejado": 925,
+        "saida": 935,
+        "diferenca": 6,
+        "entrada": 969,
+        "tempo_viagem": 38,
+        "tempo_planejado1": 45,
+        "tempo_parado1": 3,
+        "tempo_volta": 0,
+        "tempo_planejado2": 0,
+        "tempo_parado2": 0,
         "sentido": 1
     },
     {
@@ -190,16 +190,16 @@ const data = [{
         "itinerario": "Serrano/Andreazza/Marechal Floriano",
         "carro": "402",
         "reserva": "605",
-        "saida_planejado": "16:10",
-        "saida": "16:12",
-        "diferenca": "-6163:5:23:58",
-        "entrada": "16:48",
-        "tempo_viagem": "36m",
-        "tempo_planejado1": "1h 25m",
-        "tempo_parado1": null,
-        "tempo_volta": "74m",
-        "tempo_planejado2": "2h 10m",
-        "tempo_parado2": "0h 56m",
+        "saida_planejado": 970,
+        "saida": 972,
+        "diferenca": 2,
+        "entrada": 1008,
+        "tempo_viagem": 36,
+        "tempo_planejado1": 85,
+        "tempo_parado1": 0,
+        "tempo_volta": 74,
+        "tempo_planejado2": 130,
+        "tempo_parado2": 56,
         "sentido": 2
     },
     {
@@ -207,16 +207,16 @@ const data = [{
         "itinerario": "Viagem de deslocamento",
         "carro": "402",
         "reserva": "",
-        "saida_planejado": "17:35",
-        "saida": null,
-        "diferenca": null,
-        "entrada": null,
-        "tempo_viagem": null,
-        "tempo_planejado1": "25m",
-        "tempo_parado1": null,
-        "tempo_volta": null,
-        "tempo_planejado2": null,
-        "tempo_parado2": null,
+        "saida_planejado": 1055,
+        "saida": 0,
+        "diferenca": 0,
+        "entrada": 0,
+        "tempo_viagem": 0,
+        "tempo_planejado1": 25,
+        "tempo_parado1": 0,
+        "tempo_volta": 0,
+        "tempo_planejado2": 0,
+        "tempo_parado2": 0,
         "sentido": 2
     },
     {
@@ -224,16 +224,16 @@ const data = [{
         "itinerario": "Ref.Mal.Floriano/Trav.Leopoldina/Serrano",
         "carro": "402",
         "reserva": "",
-        "saida_planejado": "18:00",
-        "saida": null,
-        "diferenca": null,
-        "entrada": null,
-        "tempo_viagem": null,
-        "tempo_planejado1": "45m",
-        "tempo_parado1": null,
-        "tempo_volta": null,
-        "tempo_planejado2": "70m",
-        "tempo_parado2": null,
+        "saida_planejado": 1080,
+        "saida": 0,
+        "diferenca": 0,
+        "entrada": 0,
+        "tempo_viagem": 0,
+        "tempo_planejado1": 45,
+        "tempo_parado1": 0,
+        "tempo_volta": 0,
+        "tempo_planejado2": 70,
+        "tempo_parado2": 0,
         "sentido": 1
     },
     {
@@ -241,16 +241,16 @@ const data = [{
         "itinerario": "Serrano/Eldorado/Mal.Floriano",
         "carro": "402",
         "reserva": "",
-        "saida_planejado": "18:45",
-        "saida": null,
-        "diferenca": null,
-        "entrada": null,
-        "tempo_viagem": null,
-        "tempo_planejado1": "2h 25m",
-        "tempo_parado1": null,
-        "tempo_volta": null,
-        "tempo_planejado2": null,
-        "tempo_parado2": null,
+        "saida_planejado": 1125,
+        "saida": 0,
+        "diferenca": 0,
+        "entrada": 0,
+        "tempo_viagem": 0,
+        "tempo_planejado1": 145,
+        "tempo_parado1": 0,
+        "tempo_volta": 0,
+        "tempo_planejado2": 0,
+        "tempo_parado2": 0,
         "sentido": 2
     },
     {
@@ -258,23 +258,23 @@ const data = [{
         "itinerario": "Ref.Marechal/Travessão/Serrano/Eldorado/Iracema",
         "carro": "402",
         "reserva": "",
-        "saida_planejado": "21:10",
-        "saida": "21:11",
-        "diferenca": "-6163:5:23:59",
-        "entrada": "21:59",
-        "tempo_viagem": "48m",
-        "tempo_planejado1": "1h 30m",
-        "tempo_parado1": "42m",
-        "tempo_volta": null,
-        "tempo_planejado2": "3h 55m",
-        "tempo_parado2": null,
+        "saida_planejado": 1270,
+        "saida": 1271,
+        "diferenca": 1,
+        "entrada": 1319,
+        "tempo_viagem": 48,
+        "tempo_planejado1": 90,
+        "tempo_parado1": 42,
+        "tempo_volta": 0,
+        "tempo_planejado2": 235,
+        "tempo_parado2": 0,
         "sentido": 1
     },
 ]
 
-var svgWidth = 1100,
-    svgHeight = 400;
-var margin = { top: 40, right: 30, bottom: 40, left: 50 };
+var svgWidth = 3300,
+    svgHeight = 500;
+var margin = { top: 60, right: 30, bottom: 60, left: 50 };
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
@@ -288,11 +288,11 @@ var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var x1 = d3.scaleTime()
-    .domain(d3.extent(data, (d) => new Date("2001-01-01T" + d.saida + "Z")))
+    .domain(d3.extent(data, (d) => d.saida))
     .range([15, width]);
 
 var x2 = d3.scaleTime()
-    .domain(d3.extent(data, (d) => new Date("2001-01-01T" + d.saida + "Z")))
+    .domain(d3.extent(data, (d) => d.saida))
     .range([15, width]);
 
 //y axis width scale
@@ -308,6 +308,15 @@ var x1Generator = d3.axisBottom(x1)
 var x2Generator = d3.axisTop(x2)
 
 var yGenerator = d3.axisLeft(y)
+
+let xTickLabels = e => `${Math.floor(e / 60)}:${(e % 60).toString().padStart(2, '0')}`;
+x1Generator.ticks(50)
+x1Generator.tickSize(20)
+x1Generator.tickFormat((d) => xTickLabels(d))
+
+x2Generator.ticks(50)
+x2Generator.tickSize(20)
+x2Generator.tickFormat((d) => xTickLabels(d))
 
 //y axis tick labels
 let yTickLabels = ["AB", "BA"]
@@ -327,18 +336,21 @@ g.append("g")
 
 g.selectAll(".tick text")
     .attr("color", "#059451")
-    .attr("font-size", 13);
+    .attr("font-size", 14)
 
-var line = d3.line()
-    .x(function(d) { return x1(new Date("2001-01-01T" + d.saida + "Z")) })
-    .y(function(d) { return y(d.sentido) })
-    .curve(d3.curveCardinal);
+g.selectAll(".tick")
+    .attr("stroke-width", 0)
 
-g.append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "#059451")
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-linecap", "round")
-    .attr("stroke-width", 2)
-    .attr("d", line);
+// var line = d3.line()
+//     .x(function(d) { return x1(new Date("2001-01-01T" + d.saida + "Z")) })
+//     .y(function(d) { return y(d.sentido) })
+//     .curve(d3.curveCardinal);
+
+// g.append("path")
+//     .datum(data)
+//     .attr("fill", "none")
+//     .attr("stroke", "#059451")
+//     .attr("stroke-linejoin", "round")
+//     .attr("stroke-linecap", "round")
+//     .attr("stroke-width", 2)
+//     .attr("d", line);
