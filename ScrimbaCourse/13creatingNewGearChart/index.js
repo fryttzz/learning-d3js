a55ad2e46f8d01b7609c0adcff44e2bfcaa8dedf
Matheus Santos <matheusps0012@gmail.com>
2022-02-23 18:27:26 -0300

    Update - 23 02 2022

--- ScrimbaCourse/13creatingNewGearChart/index.js
+++ ScrimbaCourse/13creatingNewGearChart/index.js
@@ -271,12 +271,12 @@ const data = [{
         "sentido": 0
     },
 ]
-var svgWidth = 1800,
-    svgHeight = 500;
+var svgWidth = 1440,
+    svgHeight = 1000;
 
-var margin = { top: 60, right: 50, bottom: 60, left: 50 };
+var margin = { top: 50, right: 50, bottom: 80, left: 50 };
 var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+var height = svgHeight / 4 - margin.top - margin.bottom;
 var points = []
 
 for (let index = 0; index < data.length; index++) {
@@ -314,97 +314,186 @@ var mediaName = sumstat.map(d => d[0])
 
 var color = d3.scaleOrdinal().domain(mediaName).range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999'])
 
-//components group position
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+window.addEventListener('DOMContentLoaded', (event) => {
+    drawChart()
+});
 
-var x1 = d3.scaleTime()
-    .domain(d3.extent(points, (d) => d.xpoint))
-    .nice()
-    .range([15, width]);
+function drawChart() {
+    const groups = 4
+    const groupHeight = svgHeight / 4
+    const domains = [
+        [0, 360],
+        [360, 720],
+        [720, 1080],
+        [1080, 1440],
+    ]
 
-var x2 = d3.scaleTime()
-    .domain(d3.extent(points, (d) => d.xpoint))
-    .nice()
-    .range([15, width]);
+    const tickHours = [
+        [0, 60, 120, 180, 240, 300, 360],
+        [360, 420, 480, 540, 600, 660, 720],
+        [720, 780, 840, 900, 960, 1020, 1080],
+        [1080, 1140, 1200, 1260, 1320, 1380, 1440],
+    ]
 
-var y = d3.scaleLinear()
-    .domain(d3.extent(data, (d) => d.sentido))
-    .range([height, 0]);
+    const tick10Min = [
+        [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360],
+        [360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640, 650, 660, 670, 680, 690, 700, 710, 720],
+        [720, 730, 740, 750, 760, 770, 780, 790, 800, 810, 820, 830, 840, 850, 860, 870, 880, 890, 900, 910, 920, 930, 940, 950, 960, 970, 980, 990, 1000, 1010, 1020, 1030, 1040, 1050, 1060, 1070, 1080],
+        [1080, 1090, 1100, 1120, 1130, 1140, 1150, 1160, 1170, 1180, 1190, 1200, 1210, 1220, 1230, 1240, 1250, 1260, 1270, 1280, 1290, 1300, 1310, 1320, 1330, 1340, 1350, 1360, 1370, 1380, 1390, 1400, 1410, 1420, 1430, 1440],
+    ]
 
-//axies generator
-var x1Generator = d3.axisBottom(x1)
+    const positions = [0, groupHeight, groupHeight * 2, groupHeight * 3]
 
-var x2Generator = d3.axisTop(x2)
+    for (let index = 0; index < groups; index++) {
+        const g = svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + positions[index] + ")")
+            .attr("class", `chart${index + 1}`)
 
-var yGenerator = d3.axisLeft(y)
+        const gChart = g.append("g")
+            .attr("transform", "translate(" + 0 + "," + margin.top + ")")
 
-let xTickLabels = e => `${Math.floor(e / 60)}:${(e % 60).toString().padStart(2, '0')}`;
-x1Generator.tickSize(20)
-x1Generator.tickFormat((d) => xTickLabels(d))
+        const gTimeLine = g.append("g")
+            .attr("transform", "translate(" + 0 + "," + 100 + ")")
 
-x2Generator.tickSize(20)
-x2Generator.tickFormat((d) => xTickLabels(d))
+        var x1 = d3.scaleLinear()
+            .domain(domains[index])
+            .rangeRound([0, width]);
 
-//y axis tick labels
-let yTickLabels = ["A", "B"]
-yGenerator.ticks(1)
-yGenerator.tickFormat((d, i) => yTickLabels[i])
+        var x2 = d3.scaleLinear()
+            .domain(domains[index])
+            .rangeRound([0, width]);
 
-g.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(x1Generator)
+        var x3 = d3.scaleLinear()
+            .domain(domains[index])
+            .rangeRound([0, width]);
 
-g.append("g")
-    .attr("transform", "translate(0,0)")
-    .call(x2Generator)
+        var y1 = d3.scaleLinear()
+            .domain(d3.extent(data, (d) => d.sentido))
+            .range([height, 0])
 
-g.append("g")
-    .call(yGenerator)
+        var y2 = d3.scaleLinear()
+            .domain(d3.extent(data, (d) => d.sentido))
+            .range([height, 0]);
 
-g.selectAll(".tick text")
-    .attr("color", "#059451")
-    .attr("font-size", 20)
+        //axies generator
+        var x1Generator = d3.axisBottom(x1).ticks(36).tickSize(0)
+        var x2Generator = d3.axisTop(x2).ticks(36).tickSize(0)
+        var x3Generator = d3.axisTop(x3).ticks(20).tickSize(10)
 
-g.selectAll(".tick")
-    .attr("stroke-width", 0)
+        var y1Generator = d3.axisLeft(y1).ticks(1).tickSize(1)
+        var y2Generator = d3.axisRight(y2).ticks(1).tickSize(1)
 
+        //y axis tick labels
+        let yTickLabels = ["A", "B"]
+        y1Generator.tickFormat((d, i) => yTickLabels[i])
 
+        gChart.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .attr("class", "xAxis1")
+            .attr('stroke-width', 3)
+            .call(x1Generator)
 
-var line = d3.line()
-    .x((d) => d.xpoint)
-    .y((d) => d.ypoint)
-    .curve(d3.curveCatmullRom.alpha(0.30))
-    .defined(((d) => d.xpoint != 0))
+        gChart.append("g")
+            .attr("transform", "translate(0,0)")
+            .attr("class", "xAxis2")
+            .attr('stroke-width', 3)
+            .call(x2Generator)
 
-const lines = g.selectAll("lines")
-    .data(sumstat)
-    .enter()
-    .append("g");
+        gChart.append("g")
+            .attr("transform", "translate(0 ,0)")
+            .attr("class", "yAxis1")
+            .attr('stroke-width', 3)
+            .call(y1Generator)
 
-lines.append("path")
-    .attr('fill', 'none')
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-linecap", "round")
-    .attr('stroke', d => color(d[0]))
-    .attr('marker-start', 'url(#arrow)')
-    .attr('marker-mid', 'url(#arrow)')
-    .attr('marker-end', 'url(#arrow)')
-    .attr('stroke-width', 3)
-    .attr("d", (d) => {
-        return line(d[1])
-    })
+        gChart.append("g")
+            .attr("transform", "translate(" + width + ",0)")
+            .attr("class", "yAxis2")
+            .attr('stroke-width', 3)
+            .call(y2Generator)
 
-var marker = lines.append('defs')
-    .append('marker').attr('id', 'arrow')
-    .attr('viewBox', '0 0 10 10')
-    .attr('refX', 1)
-    .attr('refY', 5)
-    .attr('markerWidth', 6)
-    .attr('markerHeight', 6)
-    .attr('orient', 'auto');
+        gTimeLine.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .attr("class", "xAxis3")
+            .attr('stroke-width', 3)
+            .call(x3Generator)
 
-marker.append('path')
-    .attr("d", (d) => {
-        return line(d[1])
-    })
+        gChart.selectAll(".xAxis1 text")
+            .attr("display", "none")
+
+        gChart.selectAll(".xAxis1 .tick")
+            .attr("stroke-width", "0")
+
+        gChart.selectAll(".xAxis2 text")
+            .attr("display", "none")
+
+        gChart.selectAll(".xAxis2 .tick")
+            .attr("stroke-width", "0")
+
+        gChart.selectAll(".yAxis1 text")
+            .attr("color", "#059451")
+            .attr("font-size", 16)
+            .attr("font-weight", 700)
+
+        gChart.selectAll(".yAxis2 text")
+            .attr("display", "none")
+
+        const gridHours = gChart.selectAll("lines")
+            .data(tickHours[index])
+            .join("g")
+
+        gridHours.append("line")
+            .attr("x1", (d) => {
+                return x1(d)
+            })
+            .attr("x2", d => {
+                return x1(d)
+            })
+            .attr("y1", height)
+            .attr("y2", 0)
+            .attr("stroke-opacity", 0.7)
+            .attr("stroke", "currentColor")
+            .attr("stroke-width", 2);
+
+        const grid10Min = gChart.selectAll("lines")
+            .data(tick10Min[index])
+            .join("g")
+
+        grid10Min.append("line")
+            .attr("x1", (d) => {
+                return x1(d)
+            })
+            .attr("x2", d => {
+                return x1(d)
+            })
+            .attr("y1", height)
+            .attr("y2", 0)
+            .attr("stroke-opacity", 0.2)
+            .attr("stroke", "currentColor")
+            .attr("stroke-width", 2);
+
+        var line = d3.line()
+            .x((d) => d.xpoint)
+            .y((d) => d.ypoint)
+            .curve(d3.curveCatmullRom.alpha(0.2))
+            .defined(((d) => d.xpoint != 0))
+
+        const lines = gChart.selectAll("lines")
+            .data(sumstat)
+            .enter()
+            .append("g");
+
+        lines.append("path")
+            .attr('fill', 'none')
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr('stroke', d => color(d[0]))
+            .attr('marker-start', 'url(#arrow)')
+            .attr('marker-mid', 'url(#arrow)')
+            .attr('marker-end', 'url(#arrow)')
+            .attr('stroke-width', 3)
+            .attr("d", (d) => {
+                return line(d[1])
+            })
+    }
+
+}
