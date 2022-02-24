14b1c97d3bc20e53fe243d977749fb72290132ed
Matheus Santos <matheusps0012@gmail.com>
2022-02-24 10:05:33 -0300

    Add - Time Grid

--- ScrimbaCourse/13creatingNewGearChart/index.js
+++ ScrimbaCourse/13creatingNewGearChart/index.js
@@ -328,19 +328,36 @@ function drawChart() {
         [1080, 1440],
     ]
 
-    const tickHours = [
-        [0, 60, 120, 180, 240, 300, 360],
-        [360, 420, 480, 540, 600, 660, 720],
-        [720, 780, 840, 900, 960, 1020, 1080],
-        [1080, 1140, 1200, 1260, 1320, 1380, 1440],
-    ]
+    const tickHours = [],
+        tick10Min = [],
+        tick5Min = []
 
-    const tick10Min = [
-        [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360],
-        [360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640, 650, 660, 670, 680, 690, 700, 710, 720],
-        [720, 730, 740, 750, 760, 770, 780, 790, 800, 810, 820, 830, 840, 850, 860, 870, 880, 890, 900, 910, 920, 930, 940, 950, 960, 970, 980, 990, 1000, 1010, 1020, 1030, 1040, 1050, 1060, 1070, 1080],
-        [1080, 1090, 1100, 1120, 1130, 1140, 1150, 1160, 1170, 1180, 1190, 1200, 1210, 1220, 1230, 1240, 1250, 1260, 1270, 1280, 1290, 1300, 1310, 1320, 1330, 1340, 1350, 1360, 1370, 1380, 1390, 1400, 1410, 1420, 1430, 1440],
-    ]
+    var tickHoursCount = 60;
+    for (let i = 1; i <= 4; i++) {
+        tickHours.push([])
+        for (let j = 0; j < 6; j++) {
+            tickHours[i - 1].push(tickHoursCount);
+            tickHoursCount += 60;
+        }
+    }
+
+    var tick10MinCount = 10;
+    for (let i = 1; i <= 4; i++) {
+        tick10Min.push([])
+        for (let j = 0; j < 36; j++) {
+            tick10Min[i - 1].push(tick10MinCount);
+            tick10MinCount += 10;
+        }
+    }
+
+    var tick5MinCount = 5;
+    for (let i = 1; i <= 4; i++) {
+        tick5Min.push([])
+        for (let j = 0; j < 36; j++) {
+            tick5Min[i - 1].push(tick5MinCount);
+            tick5MinCount += 10;
+        }
+    }
 
     const positions = [0, groupHeight, groupHeight * 2, groupHeight * 3]
 
@@ -467,33 +484,63 @@ function drawChart() {
             })
             .attr("y1", height)
             .attr("y2", 0)
+            .attr("stroke-opacity", 0.35)
+            .attr("stroke", "currentColor")
+            .attr("stroke-width", 2);
+
+        const grid5Min = gChart.selectAll("lines")
+            .data(tick5Min[index])
+            .join("g")
+
+        grid5Min.append("line")
+            .attr("x1", (d) => {
+                return x1(d)
+            })
+            .attr("x2", d => {
+                return x1(d)
+            })
+            .attr("y1", 10)
+            .attr("y2", 0)
             .attr("stroke-opacity", 0.2)
             .attr("stroke", "currentColor")
             .attr("stroke-width", 2);
 
-        var line = d3.line()
-            .x((d) => d.xpoint)
-            .y((d) => d.ypoint)
-            .curve(d3.curveCatmullRom.alpha(0.2))
-            .defined(((d) => d.xpoint != 0))
-
-        const lines = gChart.selectAll("lines")
-            .data(sumstat)
-            .enter()
-            .append("g");
-
-        lines.append("path")
-            .attr('fill', 'none')
-            .attr("stroke-linejoin", "round")
-            .attr("stroke-linecap", "round")
-            .attr('stroke', d => color(d[0]))
-            .attr('marker-start', 'url(#arrow)')
-            .attr('marker-mid', 'url(#arrow)')
-            .attr('marker-end', 'url(#arrow)')
-            .attr('stroke-width', 3)
-            .attr("d", (d) => {
-                return line(d[1])
+        grid5Min.append("line")
+            .attr("x1", (d) => {
+                return x1(d)
             })
+            .attr("x2", d => {
+                return x1(d)
+            })
+            .attr("y1", height)
+            .attr("y2", height - 10)
+            .attr("stroke-opacity", 0.2)
+            .attr("stroke", "currentColor")
+            .attr("stroke-width", 2);
+
+        // var line = d3.line()
+        //     .x((d) => d.xpoint)
+        //     .y((d) => d.ypoint)
+        //     .curve(d3.curveCatmullRom.alpha(0.2))
+        //     .defined(((d) => d.xpoint != 0))
+
+        // const lines = gChart.selectAll("lines")
+        //     .data(sumstat)
+        //     .enter()
+        //     .append("g");
+
+        // lines.append("path")
+        //     .attr('fill', 'none')
+        //     .attr("stroke-linejoin", "round")
+        //     .attr("stroke-linecap", "round")
+        //     .attr('stroke', d => color(d[0]))
+        //     .attr('marker-start', 'url(#arrow)')
+        //     .attr('marker-mid', 'url(#arrow)')
+        //     .attr('marker-end', 'url(#arrow)')
+        //     .attr('stroke-width', 3)
+        //     .attr("d", (d) => {
+        //         return line(d[1])
+        //     })
     }
 
 }
